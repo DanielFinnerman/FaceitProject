149ccfe201d83aee0b00cd2cd356054327de4eb2
Daniel Finnerman <danu95d@hotmail.com>
2021-04-21 14:43:58 +0300

    validate user data

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const { UserInputError } = require('apollo-server');
 
 const User = require('../../models/User');
 
@@ -10,11 +11,19 @@ module.exports = {
     Mutation: {
         async register(_, {registerInput: {username, email, password, confirmPassword}
         }, 
-        context, info){
-            //validate user data, make sure user doesnt already exist
-            //hash pw
+				){
             //generate auth token
-            password = await bcrypt.hash(password, 12);
+
+            const user = await User.findOne({ username }); // validate user data, make sure user doesnt already exist
+            if(user) {
+              throw new UserInputError('Username is taken', {
+								errors: {
+									username: 'This username is taken'
+								}
+							})
+            }
+            
+            password = await bcrypt.hash(password, 12); // hash pw
 
             const newUser = new User({
                 email,
@@ -25,7 +34,7 @@ module.exports = {
 
             const res = await newUser.save();
 
-            const token = jwt.sign({
+            const token = jwt.sign({ // create auth token
                 id: res.id,
                 email: res.email,
                 username: res.username,                
