6dd443e90a9d120899b921e25dc59181455b2586
Daniel Finnerman <danu95d@hotmail.com>
2021-04-21 14:38:24 +0300

    first commit

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+const User = require('../../models/User');
+
+dotenv.config();
+
+module.exports = {
+    Mutation: {
+        async register(_, {registerInput: {username, email, password, confirmPassword}
+        }, 
+        context, info){
+            //validate user data, make sure user doesnt already exist
+            //hash pw
+            //generate auth token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username,                
+            }, process.env.SECRET_KEY, {expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+const port = 3000;
+
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,  useUnifiedTopology: true})
+	.then(() => {
+		console.log('MongoDB connected');
+		return server.listen(port)
+})
+	.then(res => {
+		console.log(`Server running at ${res.url}`)
+	})
