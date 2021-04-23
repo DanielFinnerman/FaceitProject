74615e9aa2c7ea5a4b856cce37e27dea3864f38e
Daniel Finnerman <danu95d@hotmail.com>
2021-04-23 15:42:49 +0300

    stats cardview

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { Card, Image } from 'semantic-ui-react'
 
-function Asd() {
-   return (
-      <div className="App">
-            <p>Stats here</p>
-      </div>
-   );
-}
-export default Asd;
+const Stats = () => (
+  <Card className="segment centered" color='orange'>
+    <Image src='https://cdn-frontend.faceit.com/web/96-1535731472/src/app/assets/images-compress/avatars/avatar_default_user_300x300.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Daniel</Card.Header>
+      <Card.Meta>Joined in 2016</Card.Meta>
+      <Card.Description>
+        Daniel is a comedian living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+ASd
+    </Card.Content>
+  </Card>
+)
+
+export default Stats
